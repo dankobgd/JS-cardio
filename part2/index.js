6f8d9395f25a8b00bd228c5d4f82d14b71621c70
Danko Petrovic <dp24031995@gmail.com>
2019-02-09 18:13:54 +0100

    longest word and array chunking

---
+++ part2/index.js
@@ -0,0 +1,52 @@
+// 1. Longest word
+
+function longestWord(sentence) {
+  const wordsArr = sentence.toLowerCase().match(/[a-z0-9]+/g);
+  const sorted = wordsArr.sort((a, b) => b.length - a.length);
+  const longest = sorted.filter(w => w.length === sorted[0].length);
+
+  if (longest.length === 1) {
+    return longest[0];
+  } else {
+    return longest;
+  }
+}
+
+// 2. Array chunking
+
+function chunkArray1(arr, size) {
+  const chunked = [];
+
+  let i = 0;
+  while (i < arr.length) {
+    chunked.push(arr.slice(i, i + size));
+    i += size;
+  }
+
+  return chunked;
+}
+
+function chunkArray2(arr, size) {
+  const chunked = [];
+  for (let i = 0; i < arr.length; i += size) {
+    chunked.push(arr.slice(i, i + size));
+  }
+
+  return chunked;
+}
+
+function chunkArray3(arr, size) {
+  const chunked = [];
+
+  arr.forEach(val => {
+    const last = chunked[chunked.length - 1];
+
+    if (!last || last.length === size) {
+      chunked.push([val]);
+    } else {
+      last.push(val);
+    }
+  });
+
+  return chunked;
+}
---
+++ part2/tasks.md
@@ -0,0 +1,37 @@
+1. ## Longest word
+
+   Return the longest word of a string
+   ex. longestWord('Hi there, my name is Brad') === 'there,'
+
+   ***
+
+2. ## Array chunking
+
+   Split an array into chunked arrays of a specific length
+   ex. chunkArray([1, 2, 3, 4, 5, 6, 7], 3) === [[1, 2, 3],[4, 5, 6],[7]]
+   ex. chunkArray([1, 2, 3, 4, 5, 6, 7], 2) === [[1, 2],[3, 4],[5, 6],[7]]
+
+   ***
+
+3. ## Flatten array
+
+   Take an array of arrays and flatten to a single array
+   ex. [[1, 2], [3, 4], [5, 6], [7]] = [1, 2, 3, 4, 5, 6, 7]
+
+   ***
+
+4. ## Anagram
+
+   Return true if anagram and false if not
+   ex. 'elbow' === 'below'
+   ex. 'Dormitory' === 'dirty room##'
+
+   ***
+
+5. ## Letter changes
+
+   Change every letter of the string to the one that follows it and capitalize the vowels
+   Z should turn to A
+   ex. 'hello there' === 'Ifmmp UIfsf'
+
+   ***
